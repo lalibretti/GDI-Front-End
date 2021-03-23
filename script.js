1d5e26567cf61eab930a5fcb98ac5cf6b5013a77
Bailey <baileylritchie@gmail.com>
2021-03-23 16:45:04 -0500

    conneted index.html and second.html to the backend

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-fetch ('http://localhost:5000/api/sample')
-.then(response => response.json())
-.then (data => console.log(data)); 
+var button = document.querySelector("#button");
+button.addEventListener("click", function getData(e) {
+  fetch ('http://localhost:5000/api/sample')
+  .then(response => response.json())
+  .then(data => console.log(data)); 
+});
+function getData(e) {
+  fetch ('http://localhost:5000/api/sample')
+  .then(response => response.json())
+  .then(data => console.log(data)); 
+}
