49041f6abda79606aadb78639a5235ff182d32ab
Lauren Libretti <lalibretti@yahoo.com>
2021-03-23 17:10:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,47 +1,56 @@
-const express = require('express');
-require('dotenv').config();
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const createError = require('http-errors');
-const asyncHandler = require('express-async-handler');
-const bodyParser = require('body-parser');
-const routes = require('./routes');
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
 
-const app = express();
-const PORT = 5000;
+http.createServer(function (request, response) {
+    console.log('request starting...');
 
-app.use(cookieParser());
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({
-  origin: ["http://localhost:3000"],
-  credentials:true
-}));
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
 
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;      
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+    }
 
-app.get('/',asyncHandler((req,res) => {
-  res.send('Hello Testing Root')
-})
-);
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end(); 
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
 
-app.use('/api',routes);
-
-/* generic 404 error */
-app.use((req, res, next) => {
-  next(createError(404,"Route not found"))
-})
-
-/* final error handler */
-app.use((error,req,res,next) => {
-  res.status(error.status || 500);
-  console.log(error);
-  res.send({
-    status: error.status,
-    message: error.message,
-    stack: error.status
-  })
-})
-
-app.listen(PORT, () => {
-  console.log(`Server is running at port:${PORT}`);
-})
+}).listen(8125);
+console.log('Server running at http://127.0.0.1:8125/');
